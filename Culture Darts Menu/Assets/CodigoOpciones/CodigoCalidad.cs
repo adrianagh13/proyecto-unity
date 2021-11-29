89079a6ff8723d4c63f5011da3652450d05339f2
paulangulo46 <19460571@colima.tecnm.mx>
2021-11-29 13:16:33 -0600

    Asset imported

---
+++ Culture Darts Menu/Assets/CodigoOpciones/CodigoCalidad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CodigoCalidad : MonoBehaviour
+{ 
+    public TMP_Dropdown dropdown;
+    public int calidad;
+    void Start()
+    {
+        calidad = PlayerPrefs.GetInt("numeroDeCalidad", 3);
+        dropdown.value = calidad;
+        AjustarCalidad();
+    }
+
+    void Update()
+    {
+        
+    }
+    public void AjustarCalidad()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value);
+        PlayerPrefs.SetInt("numeroDeCalidad", dropdown.value);
+        calidad = dropdown.value;
+    }
+}
---
+++ Culture Darts Menu/Assets/CodigoOpciones/CodigoPantallaCompleta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodigoPantallaCompleta : MonoBehaviour
+{
+    public Toggle toggle;
+
+    void Start()
+    {
+        if (Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    public void ActivarPantallaCompleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+}
